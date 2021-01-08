37b1bdb673e90c6c94a5c511c7fac7677a3b7325
Venmis <nmisiukonis430@gmail.com>
2021-01-08 16:33:36 +0000

    Update

--- A4.py
+++ A4.py
@@ -6,16 +6,18 @@ print("===========Input eq")
 z = ((2 * m * c * V**2)/(4 * R + L)**2) + 2*m**2*g*sym.sin(theta) - 2*m*k*x - 2*m*b*x2
 sym.pprint(z)
 
-def evaluate_at_equilibrium(f):
-    f.subs([(x2, 0),(V, 0)])
+equilibrium_state = ([(x2, 0),(V, 0)])
 
 print("=====Differentiated z wrt x2======")
 diff_z_x2 = z.diff(x2)
 sym.pprint(diff_z_x2)
-print("===========")
+print("=====Differentiated z wrt V======")
 diff_z_V = z.diff(V)
 sym.pprint(diff_z_V)
 print("===========")
-eq_z = evaluate_at_equilibrium(z.diff(V))
 
-sym.pprint(eq_z)
+diff_z_V_eq = diff_z_V.subs(equilibrium_state)
+sym.pprint(diff_z_V_eq)
+
+diff_z_x2_eq = diff_z_x2.subs(equilibrium_state)
+sym.pprint(diff_z_x2_eq)
