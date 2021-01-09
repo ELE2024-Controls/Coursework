136d9b41f5d7c5bf29da7efab4d1b8dfb74bec01
Venmis <nmisiukonis430@gmail.com>
2021-01-09 13:17:25 +0000

    A4 code outputing linearised form of paper version.

--- A4.py
+++ A4.py
@@ -3,7 +3,7 @@ import sympy as sym
 m, g, c, V, R, L, theta, k, b, x, x2 = \
     sym.symbols('m, g, c, V, R, L, theta, k, b, x, x2')
 print("===========Input eq")
-z = ((2 * m * c * V**2)/(4 * R + L)**2) + 2*m**2*g*sym.sin(theta) - 2*m*k*x - 2*m*b*x2
+z = (2*c*(V**2))/(m*(4*R+L)**2) + 2*g * sym.sin(theta) - 2*k/m*(x - b) - 2*b/m * x2
 sym.pprint(z)
 
 equilibrium_state = ([(x2, 0),(V, 0)])
@@ -14,10 +14,10 @@ sym.pprint(diff_z_x2)
 print("=====Differentiated z wrt V======")
 diff_z_V = z.diff(V)
 sym.pprint(diff_z_V)
-print("===========")
-
+print("====Substituting for equilibrium values")
+print("=====Substituting for Veq======")
 diff_z_V_eq = diff_z_V.subs(equilibrium_state)
 sym.pprint(diff_z_V_eq)
-
+print("=====Substituting for x2======")
 diff_z_x2_eq = diff_z_x2.subs(equilibrium_state)
 sym.pprint(diff_z_x2_eq)
