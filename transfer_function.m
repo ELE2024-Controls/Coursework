27a2b9155178e4c65dd21b4c751b5a5e1309d651
dmcc123 <73535682+dmcc123@users.noreply.github.com>
2021-01-10 01:47:02 +0000

    Matlab file to plot impulse and step function

---
+++ transfer_function.m
@@ -0,0 +1,41 @@
+
+m = 0.425
+r=0.125
+g=9.81
+d=0.42
+delta=0.65
+R=53
+L0=0.12
+L1=0.025
+alpha = 1.2
+c=6815
+k = 1880
+b=10.4
+phi = 0.733838
+
+
+xmin = d+(m*g*sin(phi)/k)
+
+xmax = delta
+
+xe = 0.75*xmin + 0.25*xmax
+
+y= delta - xe
+ 
+L=L0+(L1*exp(-delta*y))
+
+veq = sqrt((-g*sin(phi)+(k*xe/m))*exp(-2*alpha*delta)/c) * ((L0*exp(alpha*delta)) + (L1*exp(xe*alpha)) - (R*xe*exp(alpha*delta)) + (R*delta* exp(alpha*delta)) )
+
+A= 2*c/m*(R*y+L)^2*(2*veq)
+
+B= 2*k/m
+
+C=2*b/m
+
+sys = tf([A],[1, C, B])
+
+
+subplot(2,1,1)
+step(sys)
+subplot(2,1,2)
+impulse(sys)
